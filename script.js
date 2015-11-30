e082e4bfe1d89d1ca1e78463ab0cdf691925d5a6
Ross E <ross.eastman@mayden.co.uk>
2015-11-30 15:43:30 +0000

    added messages after inputs

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 $(function() {
-    //document.getElementsByName('header1')[0].innerHTML = "!Changed it by name!";
-    $('[name = "header1"]').html("!Changed it by name!");
-    //document.getElementById('cebi').innerHTML = "!Changed by id!";
-    $('#cebi').html("!Changed it by id!");
-    //document.getElementsByClassName('form-group class')[0].innerHTML = "!Changed by classname!";
-    $('.form-group.class').html("!classname!");
-    //document.getElementsByTagName('label')[6].innerHTML = "!Changed by tagname!";
 
-    $("label").eq(1).html("!Changed by tagname!");
+    $('[type=text]').after("<p>!This is a message!</p>");
+    $('[type=email]').after("<p>!This is a message!</p>");
+    $('.radio').after("<p>!This is a message!</p>");
 
 })
