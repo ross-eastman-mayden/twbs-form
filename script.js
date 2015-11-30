3b38fdb05a9b9858446aafb70340e6db08609bb2
Ross E <ross.eastman@mayden.co.uk>
2015-11-30 16:02:40 +0000

    oneline wonder

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(function() {
 
-    $('[type=text]').after("<p>!This is a message!</p>");
-    $('[type=email]').after("<p>!This is a message!</p>");
-    $('.radio').after("<p>!This is a message!</p>");
+    $('.form-group,.radio').not('#button').append("<p>!this is a massage!</p>")
 
 })
