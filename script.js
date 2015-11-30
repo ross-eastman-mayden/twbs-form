587706d17f887fe4aae17782a6bd86cbc2999e77
Ross E <ross.eastman@mayden.co.uk>
2015-11-30 14:52:17 +0000

    added jquery for onload etc

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("troll");
+$(function() {
+    document.getElementsByName('header1')[0].innerHTML = "!Changed it by name!";
+document.getElementById('cebi').innerHTML = "!Changed by id!";
+document.getElementsByClassName('form-group class')[0].innerHTML = "!Changed by classname!";
+document.getElementsByTagName('label')[6].innerHTML = "!Changed by tagname!";
+})
