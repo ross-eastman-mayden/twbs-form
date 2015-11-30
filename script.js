1f1cb6c55565bd8b3c437d81d374180d1b01342c
Ross E <ross.eastman@mayden.co.uk>
2015-11-30 16:55:39 +0000

    added on submit func

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(function() {
-
+    $('form').submit(function())
+    {
+        console.log('submitting!')
+    })
     $('.form-group,.radio').not('#button').append("<p>!this is a massage!</p>")
 
 })
