7cdcee2358b8634887b613486f7acbbf7efceba3
Ross E <ross.eastman@mayden.co.uk>
2015-11-30 15:10:32 +0000

    used jquery

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(function() {
-    document.getElementsByName('header1')[0].innerHTML = "!Changed it by name!";
-document.getElementById('cebi').innerHTML = "!Changed by id!";
-document.getElementsByClassName('form-group class')[0].innerHTML = "!Changed by classname!";
-document.getElementsByTagName('label')[6].innerHTML = "!Changed by tagname!";
+    //document.getElementsByName('header1')[0].innerHTML = "!Changed it by name!";
+    $('[name = "header1"]').html("!Changed it by name!");
+    //document.getElementById('cebi').innerHTML = "!Changed by id!";
+    $('#cebi').html("!Changed it by id!");
+    //document.getElementsByClassName('form-group class')[0].innerHTML = "!Changed by classname!";
+    $('.form-group.class').html("!classname!");
+    //document.getElementsByTagName('label')[6].innerHTML = "!Changed by tagname!";
+
+    $("label").eq(1).html("!Changed by tagname!");
+
 })
