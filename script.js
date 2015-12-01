2d09e9537cbda93b12f32a9490e66b71af5dd527
Ross E <ross.eastman@mayden.co.uk>
2015-12-01 17:06:01 +0000

    lol

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-$(function() {
-    $('form').submit(function()
-    {
+$(function () {
 
+        $('form').submit(function () {
+            $('form').trigger('dom')
+            return false
+        })
+
+    $('form').on('dom', function () {
         //console.log('submitting!')
-
-        if(!$('#required').val()) {
+        if (!$('#required').val()) {
+            $('#required').trigger('focus')
             $('#required').after("<p>!INSERT SUMMIT!</p>")
             console.log("false")
-            $('#required').trigger('focus')
+
             return false;
         }
         else {
             console.log("true")
             return true;
         }
+        //$('.form-group,.radio').not('#button').append("<p>!this is a massage!</p>")
     })
-    //$('.form-group,.radio').not('#button').append("<p>!this is a massage!</p>")
+
 
 })
