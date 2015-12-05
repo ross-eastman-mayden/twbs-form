d74031333557131f19f30f01af66dd3c033d00fa
Ross E <ross.eastman@mayden.co.uk>
2015-12-05 11:32:15 +0000

    fade in and out validation on load

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 $(function() {
-    $('form').submit(function())
+    $('form').submit(function()
     {
         console.log('submitting!')
+        alert("please fill in '$('required')'");
 
-        /*if...
-        return false;*/
-    })
-    $('.form-group,.radio').not('#button').append("<p>!this is a massage!</p>")
+    });
+
+    var message = $("<p class='message'>!this is a massage!</p>").css({"display": "none"})
+    $('.form-group,.radio').not('#button').append(message);
+    $(".message").fadeIn()
+
+
+    var timedFade = function () {
+        $(".message").fadeOut(3000)
+    };
+
+    setTimeout(function() {
+        timedFade()
+    },
+        2000);
+
 
 })
