66764d38a342d5dea2ae2620c06db26a40a9ada7
Ross Eastman <ross.eastman@mayden.co.uk>
2015-12-10 21:12:41 +0000

    starting to get somewhere

--- validation.js
+++ validation.js
@@ -1,16 +1,33 @@
-var htmlWarning = "html warning"
+$(function () {
+    var requiredWarning = '<p class="requiredWarning">This Field Is Required!</p>'
 
-console.log("please print something")
+    console.log("please print something")
 
-$("form").submit(function (e) {
-    console.log("submitting")
-    e.preventDefault()
-    if (!$('.required').val()) {
-        $('.required').parents('p').append('htmlWarning')
-        console.log('required!')
-        e.preventDefault()
-    } else {
-        console.log('well done mate')
-        return true;
-    }
+    $("form").submit(function (e) {
+        console.log("submitting")
+
+        if ($('radio[value="1"]').is(":checked")) {
+            console.log('val 1 is checked')
+            $('#requiredIfChecked').parent().addClass("required")
+        } else {
+            $('#requiredIfChecked .required').remove()
+        }
+
+        $(".requiredWarning").remove()
+
+        $('.required').each(function () {
+            if (!$('.required').val()) {
+                $('.required').each(function () {
+                    $('.required').parent().append(requiredWarning)
+                    console.log('required!')
+                    e.preventDefault()
+                })
+            } else {
+                console.log('well done mate')
+                return true
+            }
+        })
+
+
+    })
 })
